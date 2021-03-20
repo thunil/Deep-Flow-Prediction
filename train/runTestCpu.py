37c1f6a623e9ea77b542a478bf934eb214c11288
NT <nils.thuerey@tum.de>
2021-03-20 15:20:59 +0800

    added convenience script for CPU testing

---
+++ train/runTestCpu.py
@@ -0,0 +1,217 @@
+################
+#
+# Deep Flow Prediction - N. Thuerey, K. Weissenov, L. Prantl, X. Hu (TUM)
+#
+# This file is a copy of runTest.py, which - for convenience - can be run
+# on a CPU. This is convenient for quick testing on non-GPU machines.
+# If you have a GPU, the runTest.py script is strongly recommended instead.
+#
+################
+
+import os,sys,random,math
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+
+from dataset import TurbDataset
+import utils
+from utils import log
+
+suffix = "" # customize loading & output if necessary
+prefix = ""
+if len(sys.argv)>1:
+    prefix = sys.argv[1]
+    print("Output prefix: {}".format(prefix))
+
+expo = 3
+dataset = TurbDataset(None, mode=TurbDataset.TEST, dataDirTest="../data/test/")
+testLoader = DataLoader(dataset, batch_size=1, shuffle=False)
+
+targets = torch.FloatTensor(1, 3, 128, 128)
+targets = Variable(targets)
+inputs = torch.FloatTensor(1, 3, 128, 128)
+inputs = Variable(inputs)
+
+targets_dn = torch.FloatTensor(1, 3, 128, 128)
+targets_dn = Variable(targets_dn)
+outputs_dn = torch.FloatTensor(1, 3, 128, 128)
+outputs_dn = Variable(outputs_dn)
+
+def blockUNet(in_c, out_c, name, transposed=False, bn=True, relu=True, size=4, pad=1, dropout=0.):
+    block = nn.Sequential()
+
+    if relu:
+        block.add_module('%s_relu' % name, nn.ReLU(inplace=True))
+    else:
+        block.add_module('%s_leakyrelu' % name, nn.LeakyReLU(0.2, inplace=True))
+
+    if not transposed:
+        block.add_module('%s_conv' % name, nn.Conv2d(in_c, out_c, kernel_size=size, stride=2, padding=pad, bias=True))
+    else:
+        block.add_module('%s_upsam' % name, nn.Upsample(scale_factor=2, mode='bilinear'))
+        # reduce kernel size by one for the upsampling (ie decoder part)
+        block.add_module('%s_tconv' % name, nn.Conv2d(in_c, out_c, kernel_size=(size-1), stride=1, padding=pad, bias=True))
+
+    if bn:
+        block.add_module('%s_bn' % name, nn.BatchNorm2d(out_c))
+    if dropout>0.:
+        block.add_module('%s_dropout' % name, nn.Dropout2d( dropout, inplace=True))
+
+    return block
+    
+class DfpNet(nn.Module):
+    def __init__(self, channelExponent=6, dropout=0.):
+        super(DfpNet, self).__init__()
+        channels = int(2 ** channelExponent + 0.5)
+
+        self.layer1 = nn.Sequential()
+        self.layer1.add_module('layer1', nn.Conv2d(3, channels, 4, 2, 1, bias=True))
+
+        self.layer2 = blockUNet(channels  , channels*2, 'layer2', transposed=False, bn=True,  relu=False, dropout=dropout )
+        self.layer2b= blockUNet(channels*2, channels*2, 'layer2b',transposed=False, bn=True,  relu=False, dropout=dropout )
+        self.layer3 = blockUNet(channels*2, channels*4, 'layer3', transposed=False, bn=True,  relu=False, dropout=dropout )
+        self.layer4 = blockUNet(channels*4, channels*8, 'layer4', transposed=False, bn=True,  relu=False, dropout=dropout ,  size=4 ) # note, size 4!
+        self.layer5 = blockUNet(channels*8, channels*8, 'layer5', transposed=False, bn=True,  relu=False, dropout=dropout , size=2,pad=0)
+        self.layer6 = blockUNet(channels*8, channels*8, 'layer6', transposed=False, bn=False, relu=False, dropout=dropout , size=2,pad=0)
+     
+        # note, kernel size is internally reduced by one for the decoder part
+        self.dlayer6 = blockUNet(channels*8, channels*8, 'dlayer6', transposed=True, bn=True, relu=True, dropout=dropout , size=2,pad=0)
+        self.dlayer5 = blockUNet(channels*16,channels*8, 'dlayer5', transposed=True, bn=True, relu=True, dropout=dropout , size=2,pad=0)
+        self.dlayer4 = blockUNet(channels*16,channels*4, 'dlayer4', transposed=True, bn=True, relu=True, dropout=dropout ) 
+        self.dlayer3 = blockUNet(channels*8, channels*2, 'dlayer3', transposed=True, bn=True, relu=True, dropout=dropout )
+        self.dlayer2b= blockUNet(channels*4, channels*2, 'dlayer2b',transposed=True, bn=True, relu=True, dropout=dropout )
+        self.dlayer2 = blockUNet(channels*4, channels  , 'dlayer2', transposed=True, bn=True, relu=True, dropout=dropout )
+
+        self.dlayer1 = nn.Sequential()
+        self.dlayer1.add_module('dlayer1_relu', nn.ReLU(inplace=True))
+        self.dlayer1.add_module('dlayer1_tconv', nn.ConvTranspose2d(channels*2, 3, 4, 2, 1, bias=True))
+
+    def forward(self, x):
+        out1 = self.layer1(x)
+        out2 = self.layer2(out1)
+        out2b= self.layer2b(out2)
+        out3 = self.layer3(out2b)
+        out4 = self.layer4(out3)
+        out5 = self.layer5(out4)
+        out6 = self.layer6(out5)
+        dout6 = self.dlayer6(out6)
+        dout6_out5 = torch.cat([dout6, out5], 1)
+        dout5 = self.dlayer5(dout6_out5)
+        dout5_out4 = torch.cat([dout5, out4], 1)
+        dout4 = self.dlayer4(dout5_out4)
+        dout4_out3 = torch.cat([dout4, out3], 1)
+        dout3 = self.dlayer3(dout4_out3)
+        dout3_out2b = torch.cat([dout3, out2b], 1)
+        dout2b = self.dlayer2b(dout3_out2b)
+        dout2b_out2 = torch.cat([dout2b, out2], 1)
+        dout2 = self.dlayer2(dout2b_out2)
+        dout2_out1 = torch.cat([dout2, out1], 1)
+        dout1 = self.dlayer1(dout2_out1)
+        return dout1
+netG = DfpNet(channelExponent=expo)
+
+# ---
+
+lf = "./" + prefix + "testout{}.txt".format(suffix) 
+utils.makeDirs(["results_test"])
+
+# loop over different trained models
+avgLoss = 0.
+losses = []
+models = []
+
+for si in range(25):
+    s = chr(96+si)
+    if(si==0): 
+        s = "" # check modelG, and modelG + char
+    modelFn = "./" + prefix + "modelG{}{}".format(suffix,s)
+    if not os.path.isfile(modelFn):
+        continue
+
+    models.append(modelFn)
+    log(lf, "Loading " + modelFn )
+    netG.load_state_dict( torch.load(modelFn) )
+    log(lf, "Loaded " + modelFn )
+
+    criterionL1 = nn.L1Loss()
+    L1val_accum = 0.0
+    L1val_dn_accum = 0.0
+    lossPer_p_accum = 0
+    lossPer_v_accum = 0
+    lossPer_accum = 0
+
+    netG.eval()
+
+    for i, data in enumerate(testLoader, 0):
+        inputs_cpu, targets_cpu = data
+        targets_cpu, inputs_cpu = targets_cpu.float(), inputs_cpu.float()
+        inputs.data.resize_as_(inputs_cpu).copy_(inputs_cpu)
+        targets.data.resize_as_(targets_cpu).copy_(targets_cpu)
+
+        outputs = netG(inputs)
+        outputs_cpu = outputs.data.cpu().numpy()[0]
+        targets_cpu = targets_cpu.cpu().numpy()[0]
+
+        lossL1 = criterionL1(outputs, targets)
+        L1val_accum += lossL1.item()
+
+        # precentage loss by ratio of means which is same as the ratio of the sum
+        lossPer_p = np.sum(np.abs(outputs_cpu[0] - targets_cpu[0]))/np.sum(np.abs(targets_cpu[0]))
+        lossPer_v = ( np.sum(np.abs(outputs_cpu[1] - targets_cpu[1])) + np.sum(np.abs(outputs_cpu[2] - targets_cpu[2])) ) / ( np.sum(np.abs(targets_cpu[1])) + np.sum(np.abs(targets_cpu[2])) )
+        lossPer = np.sum(np.abs(outputs_cpu - targets_cpu))/np.sum(np.abs(targets_cpu))
+        lossPer_p_accum += lossPer_p.item()
+        lossPer_v_accum += lossPer_v.item()
+        lossPer_accum += lossPer.item()
+
+        log(lf, "Test sample %d"% i )
+        log(lf, "    pressure:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs_cpu[0] - targets_cpu[0])), lossPer_p.item()) )
+        log(lf, "    velocity:  abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs_cpu[1] - targets_cpu[1])) + np.sum(np.abs(outputs_cpu[2] - targets_cpu[2])) , lossPer_v.item() ) )
+        log(lf, "    aggregate: abs. difference, ratio: %f , %f " % (np.sum(np.abs(outputs_cpu    - targets_cpu   )), lossPer.item()) )
+
+        # Calculate the norm
+        input_ndarray = inputs_cpu.cpu().numpy()[0]
+        v_norm = ( np.max(np.abs(input_ndarray[0,:,:]))**2 + np.max(np.abs(input_ndarray[1,:,:]))**2 )**0.5
+
+        outputs_denormalized = dataset.denormalize(outputs_cpu, v_norm)
+        targets_denormalized = dataset.denormalize(targets_cpu, v_norm)
+
+        # denormalized error 
+        outputs_denormalized_comp=np.array([outputs_denormalized])
+        outputs_denormalized_comp=torch.from_numpy(outputs_denormalized_comp)
+        targets_denormalized_comp=np.array([targets_denormalized])
+        targets_denormalized_comp=torch.from_numpy(targets_denormalized_comp)
+
+        targets_denormalized_comp, outputs_denormalized_comp = targets_denormalized_comp.float(), outputs_denormalized_comp.float()
+
+        outputs_dn.data.resize_as_(outputs_denormalized_comp).copy_(outputs_denormalized_comp)
+        targets_dn.data.resize_as_(targets_denormalized_comp).copy_(targets_denormalized_comp)
+
+        lossL1_dn = criterionL1(outputs_dn, targets_dn)
+        L1val_dn_accum += lossL1_dn.item()
+
+        # write output image, note - this is currently overwritten for multiple models
+        os.chdir("./results_test/")
+        utils.imageOut("%04d"%(i), outputs_cpu, targets_cpu, normalize=False, saveMontage=True) # write normalized with error
+        os.chdir("../")
+
+    log(lf, "\n") 
+    L1val_accum     /= len(testLoader)
+    lossPer_p_accum /= len(testLoader)
+    lossPer_v_accum /= len(testLoader)
+    lossPer_accum   /= len(testLoader)
+    L1val_dn_accum  /= len(testLoader)
+    log(lf, "Loss percentage (p, v, combined): %f %%    %f %%    %f %% " % (lossPer_p_accum*100, lossPer_v_accum*100, lossPer_accum*100 ) )
+    log(lf, "L1 error: %f" % (L1val_accum) )
+    log(lf, "Denormalized error: %f" % (L1val_dn_accum) )
+    log(lf, "\n") 
+
+    avgLoss += lossPer_accum
+    losses.append(lossPer_accum)
+
+if len(losses)>1:
+    avgLoss /= len(losses)
+    lossStdErr = np.std(losses) / math.sqrt(len(losses))
+    log(lf, "Averaged relative error and std dev:   %f , %f " % (avgLoss,lossStdErr) )
+
